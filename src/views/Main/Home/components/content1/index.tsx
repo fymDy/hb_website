ca7bdd12a1a08d2e04a03662f41a33facea3194e
mark <mark995@gmail.com>
2024-08-27 16:02:44 +0400

    content1

--- src/views/Main/Home/components/content1/index.tsx
+++ src/views/Main/Home/components/content1/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: Mark
  * @Date: 2024-08-23 16:12:25
- * @LastEditTime: 2024-08-27 14:26:30
+ * @LastEditTime: 2024-08-27 15:50:09
  * @LastEditors: MarkMark
  * @Description: 佛祖保佑无bug
  * @FilePath: /hb_website/src/views/Main/Home/components/content1/index.tsx
@@ -23,51 +23,53 @@ const Home: React.FC = () => {
   </defs>
   </svg>
   const textStr=[   'Innovation','Integrity','Collaboration','Excellence']
-  return <div className='Content1 container  w-[calc(100%-270px)] lg:h-[638px] mx-auto mx-[135px] mt-[90px] mb-[134px] '>
+  return <div className='Content1 container w-full lg:w-[calc(100%-270px)] mx-5 lg:h-[638px] mx-auto lg:mx-[135px] lg:mt-[90px] lg:mb-[134px] '>
      
-      <div className="relative container h-full w-full flex     justify-between items-center ">
-           {/* 图片部分 */}
-          <div className="relative w-2/3 h-full ">
-            <img
-              src={img1}
-              alt="Image 1"
-              className="w-[327px] h-[297px] absolute left-0 bottom-[111px] z-10"
-            />
-            <img
-              src={img2}
-              alt="Image 2"
-              className="w-[298px] h-[357px] absolute left-[270px] bottom-[170px] z-20 bg-[#000]"
-            />
-            <img
-              src={img3}
-              alt="Image 3"
-                className="w-[326px] h-[269px] absolute left-[500px] bottom-[206px] z-10"
-            />
-          </div>
-           {/* 文本内容部分 */}
-      <div className=" lg:w-[690px] h-full flex flex-col justify-between ">
-        <h2 className=" lg:text-[56px] font-semibold text-[#2f3e46] ">
-          Your Success, Our Commitment
-        </h2>
-        <p className=" lg:text-[24px] font-normal text-[#5e6472] ">
-          Welcome to WebMatrix, a company dedicated to innovation and excellence in software development. We are customer-centric and provide high-quality software solutions that help businesses achieve digital transformation and growth.
-        </p>
-        <p className="lg:text-[24px] font-normal text-[#5e6472]">
-          We have core values that guide our work and relationships:
-        </p>
-        <ul className="lg:text-[24px] text-[#5e6472] my-3  gap-4 grid grid-cols-2 ">
-          {
-            textStr?.map((item:string,i:number)=> <li key={i} className='flex lg:w-[290px] gap-4 flex-wrap'>
-            {iconDom}
-            <span>{item}</span>
-        </li>)
-          }
-        </ul>
-        <button className="lg:w-[170px] lg:h-[56px] lg:text-5 lg:rounded-xl bg-green-500 text-white py-2 px-6  hover:bg-green-600">
-           Contact Us
-        </button>
-      </div>
-      </div>
+     <div className="relative container  w-[calc(100%-20px)] flex flex-col lg:flex-row lg:justify-between lg:items-center">
+  {/* 图片部分 */}
+  <div className="relative container
+   mx-auto w-full  h-[268px]  md:h-[488px]  lg:w-[990px] ">
+    <img
+      src={img1}
+      alt="Image 1"
+      className="w-[146px] h-[180px] md:w-[274px] md:h-[297px]  lg:w-[327px] lg:h-[297px] absolute left-0  bottom-0 lg:left-0 lg:bottom-[111px] z-10"
+    />
+    <img
+      src={img2}
+      alt="Image 2"
+      className="w-[133px] h-[216px]  lg:w-[298px] lg:h-[357px] absolute left-[120px] bottom-[36px] md:left-[226px] md:bottom-[59px]  lg:left-[270px] lg:bottom-[170px] z-20 bg-[#000]"
+    />
+    <img
+      src={img3}
+      alt="Image 3"
+      className="w-[145px] h-[163px]  md:w-[274px] md:h-[270px]  lg:w-[326px] lg:h-[269px] absolute left-[253px] bottom-[58px]  md:left-[445px] md:bottom-[96px] lg:left-[500px] lg:bottom-[206px] z-10"
+    />
+  </div>
+
+  {/* 文本内容部分 */}
+  <div className="flex-1 lg:w-[690px] h-full flex flex-col justify-between">
+    <h2 className="text-[32px] lg:text-[56px] font-semibold text-[#2f3e46]">
+      Your Success, Our Commitment
+    </h2>
+    <p className="text-[20px] md:text-[24px]   lg:text-[24px] font-normal text-[#5e6472]">
+      Welcome to WebMatrix, a company dedicated to innovation and excellence in software development. We are customer-centric and provide high-quality software solutions that help businesses achieve digital transformation and growth.
+    </p>
+    <p className="text-[20px]  md:text-[24px] lg:text-[24px] font-normal text-[#5e6472]">
+      We have core values that guide our work and relationships:
+    </p>
+    <ul className="text-[20px]  md:text-[24px] lg:text-[24px] text-[#5e6472] my-3 gap-4 grid grid-cols-1 lg:grid-cols-2">
+      {textStr?.map((item: string, i: number) => (
+        <li key={i} className="flex lg:w-[290px] gap-4 flex-wrap">
+          {iconDom}
+          <span>{item}</span>
+        </li>
+      ))}
+    </ul>
+    <button className="w-full lg:w-[170px] lg:h-[56px] text-[20px]  md:text-[24px] lg:text-[24px]  lg:rounded-xl bg-green-500 text-white mt-16 hover:bg-green-600">
+      Contact Us
+    </button>
+  </div>
+</div>
 
      
   </div>;
